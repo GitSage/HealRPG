da3852130c3dc796801f31cfc2977c59d034a2f2
lajinnsolo <waerstein@gmail.com>
2016-09-03 10:28:00 -0600

    Added spellService and spells to GameSpellPanelComponent

--- src/app/game-panel/game-spell-panel/game-spell-panel.component.ts
+++ src/app/game-panel/game-spell-panel/game-spell-panel.component.ts
@@ -1,17 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { SpellService } from '../../shared/spell/spell.service';
+import { Spell } from '../../shared/spell/spell';
 
 @Component({
   moduleId: module.id,
   selector: 'game-spell-panel',
   templateUrl: 'game-spell-panel.component.html',
   styleUrls: ['game-spell-panel.component.css'],
+  providers: [ SpellService ],
   directives: []
 })
 export class GameSpellPanelComponent implements OnInit {
 
-  constructor() {}
+  private spells : Spell[];
+  constructor(private spellService : SpellService) {}
 
-  ngOnInit() {
+  ngOnInit(){
   }
 
 }
