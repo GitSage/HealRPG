324adbc776e356f481b2755335d51d0115038149
lajinnsolo <waerstein@gmail.com>
2016-09-05 23:27:28 -0600

    Reverted accidental removal of tests

--- src/app/game-panel/shared/health-bar/health-bar.component.spec.ts
+++ src/app/game-panel/shared/health-bar/health-bar.component.spec.ts
@@ -1,18 +1,46 @@
 import {
+  beforeEach,
   beforeEachProviders,
   describe,
   expect,
   it,
   inject,
 } from '@angular/core/testing';
+import { ComponentFixture, TestComponentBuilder } from '@angular/compiler/testing';
+import { Component } from '@angular/core';
+import { By } from '@angular/platform-browser';
 import { HealthBarComponent } from './health-bar.component';
 
 describe('Component: HealthBar', () => {
+  let builder: TestComponentBuilder;
 
   beforeEachProviders(() => [HealthBarComponent]);
+  beforeEach(inject([TestComponentBuilder], function (tcb: TestComponentBuilder) {
+    builder = tcb;
+  }));
 
   it('should inject the component', inject([HealthBarComponent],
       (component: HealthBarComponent) => {
     expect(component).toBeTruthy();
   }));
+
+  it('should create the component', inject([], () => {
+    return builder.createAsync(HealthBarComponentTestController)
+      .then((fixture: ComponentFixture<any>) => {
+        let query = fixture.debugElement.query(By.directive(HealthBarComponent));
+        expect(query).toBeTruthy();
+        expect(query.componentInstance).toBeTruthy();
+      });
+  }));
 });
+
+@Component({
+  selector: 'test',
+  template: `
+    <app-health-bar></app-health-bar>
+  `,
+  directives: [HealthBarComponent]
+})
+class HealthBarComponentTestController {
+}
+
