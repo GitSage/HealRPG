6fe5db50aef1c7b3f586a4234d286fc52ea8cdc4
lajinnsolo <waerstein@gmail.com>
2016-09-27 22:00:15 -0600

    Added constructor to spell

--- src/app/shared/spell/mock-spell.ts
+++ src/app/shared/spell/mock-spell.ts
@@ -1,8 +1,8 @@
 import { Spell } from './spell';
 
 export var MOCK_SPELLS:Spell[] = [
-    { id:1, name:"Heal", targetTypes:["single"], power:100, cost:50, active: false },
-    { id:2, name:"Quick Heal", targetTypes:["single"], power:50, cost:50, active: false },
-    { id:3, name:"Renew", targetTypes:["single recurring"], power:20, cost:50, active: false },
-    { id:4, name:"Group Heal", targetTypes:["group"], power:50, cost:50, active: false }
+  new Spell(1, "Heal", ["single"], 100, 50, true),
+  new Spell(2, "Quick Heal", ["single"], 50, 50, false),
+  new Spell(3, "Renew", ["single recurring"], 20, 50, false),
+  new Spell(4, "Group Heal", ["group"], 50, 50, false)
 ];
--- src/app/shared/spell/spell.ts
+++ src/app/shared/spell/spell.ts
@@ -4,5 +4,21 @@ export class Spell {
     targetTypes: string[];
     power: number;
     cost: number;
+    cooldownPercentage: number;
     active: boolean;
+
+  constructor(id:number,
+              name:string,
+              targetTypes:string[],
+              power:number,
+              cost:number,
+              active:boolean){
+    this.id = id;
+    this.name = name;
+    this.targetTypes = targetTypes;
+    this.power = power;
+    this.cost = cost;
+    this.cooldownPercentage = 0;
+    this.active = active;
+  }
 }
